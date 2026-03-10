2af0b87e4edf4d2a97e8daad57390d89fd637473
Sunny Sawrav <sunny.sawrav@gmail.com>
2026-03-11 01:10:45 +0530

    add sanity deployment appId

--- studio/sanity.cli.js
+++ studio/sanity.cli.js
@@ -10,6 +10,7 @@ export default defineCliConfig({
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
+    appId: 'j4exwrdpvwe5bft41qjbz6a9',
     autoUpdates: true,
   }
 })
